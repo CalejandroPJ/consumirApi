f52cd6ab813f8d4c3fbc79cc9672b1e3baae4e14
CalejandroPJ <carlos.penaj@upb.edu.co>
2025-08-27 19:38:32 -0500

    Consumo Api
    Api para consumir

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f9913bf341e499cf100b14ccc30a3edebb1e38b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c797e87d4a9825ee08a21d16798fd28d3b5500eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
